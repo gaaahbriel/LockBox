b786df233452943049c330a4664c3af25ce3877f
Gaaahbriel <gabriel.bonfim111@gmail.com>
2026-03-27 15:40:17 -0300

    Reutilização de código

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'entities',
+        'user' => 'root',
+        'pass' => ''
+    ]
+];
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+    require '../models/Usuario.php';
+
+    session_start();
+
+    require '../functions.php';
+    require '../Flash.php';
+    require '../Database.php';
+    require '../Validacao.php';
+    require '../routes.php';
