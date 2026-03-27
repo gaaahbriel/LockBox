9ae7e40688e0360967c0a587ecbd8a398efdefd5
Gaaahbriel <gabriel.bonfim111@gmail.com>
2026-03-27 16:05:16 -0300

    Criança o index

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -1,4 +1,2 @@
 <?php
-$livros = Livro::all($_REQUEST['pesquisar'] ?? '');
-
-view('index', compact('livros'));
+view('index');
--- views/index.view.php
+++ views/index.view.php
@@ -1,1 +1,12 @@
-LockBox
+<div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content lg:flex-row">
+        <div>
+            <p class="py-4">Bem vindo ao</p>
+            <h1 class="text-5xl font-bold">LockBox</h1>
+            <p class="py-4">
+                Onde você guarda <span class="italic">tudo</span> com segurança.
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
+    </div>
+</div>
